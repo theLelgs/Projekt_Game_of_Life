f35833d149d414dc437d17420c5f5c1457f7af39
Linus Lindström <linus@datasnillet.com>
2025-11-22 15:23:21 +0100

    Swapped output to raylib

--- Projekt_Game_of_Life/Program.cs
+++ Projekt_Game_of_Life/Program.cs
@@ -12,18 +12,32 @@ for (int y=0;y<10;y++)
 { 
     for (int x = 0; x<10; x++)
     {
-        Console.CursorLeft=x;
-        Console.CursorTop=y;
         if (Random.Shared.Next(2)==0)
         {
             IsAlive[x,y]=true;
-        }
-        if (IsAlive[x,y]==true)
-        {
-            Console.Write("E");
         }
     }
 }
 
 
-Console.ReadLine();
+
+while (!Raylib.WindowShouldClose())
+{
+    Raylib.BeginDrawing();
+    Raylib.ClearBackground(Color.Black);
+
+    for (int y=0;y<10;y++)
+    { 
+        for (int x = 0; x<10; x++)
+        {
+            if (IsAlive[x,y])
+            {
+                Raylib.DrawRectangle(x*25,y*25,25,25,Color.White);
+            }
+        }
+    }
+
+
+    Raylib.EndDrawing();
+}
+
